26f9136bf9629dbab5b937fb8665dde3f7c631c7
Sullyvan Boulanger <sullyvanboulanger@gmail.com>
2024-02-06 22:16:22 +0100

    init: Initialize project

---
+++ src/test/java/com/forceofcollection/foc/FocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forceofcollection.foc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
