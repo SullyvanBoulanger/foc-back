6c016a4e1a2e3f047689b843d280ad91b4fc2393
Sullyvan Boulanger <sullyvanboulanger@gmail.com>
2024-02-13 11:05:56 +0100

    feat: Implement Card Endpoint

--- src/main/java/com/forceofcollection/foc/controller/CardController.java
+++ src/main/java/com/forceofcollection/foc/controller/CardController.java
@@ -3,8 +3,24 @@ package com.forceofcollection.foc.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.forceofcollection.foc.entity.Card;
+import com.forceofcollection.foc.repository.CardRepository;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api/card")
 public class CardController {
+
+    @Autowired
+    private CardRepository cardRepository;
     
+    @GetMapping("/{id}")
+    public Optional<Card> getCardById(@PathVariable Integer id) {
+        return cardRepository.findById(id);
+    }
 }
