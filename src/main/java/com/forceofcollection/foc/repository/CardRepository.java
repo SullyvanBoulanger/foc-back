2726d2c79c0c4df6a40de65c5e967fa9b3e1971c
Sullyvan Boulanger <sullyvanboulanger@gmail.com>
2024-02-13 11:05:56 +0100

    feat: Implement Card Endpoint

---
+++ src/main/java/com/forceofcollection/foc/repository/CardRepository.java
@@ -0,0 +1,11 @@
+package com.forceofcollection.foc.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.forceofcollection.foc.entity.Card;
+
+public interface CardRepository extends JpaRepository<Card, Integer>{
+    Optional<Card> findById(Integer id);
+}
