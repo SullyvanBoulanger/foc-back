190de37e993159490846230a67742ea486ae3d94
Sullyvan Boulanger <sullyvanboulanger@gmail.com>
2024-02-21 14:24:58 +0100

    fix: Remove Token expiration

--- src/main/java/com/forceofcollection/foc/service/implementation/JwtServiceImpl.java
+++ src/main/java/com/forceofcollection/foc/service/implementation/JwtServiceImpl.java
@@ -35,7 +35,7 @@ public class JwtServiceImpl implements JwtService {
     @Override
     public boolean isTokenValid(String token, UserDetails userDetails) {
         final String userName = extractUserName(token);
-        return (userName.equals(userDetails.getUsername())) && !isTokenExpired(token);
+        return (userName.equals(userDetails.getUsername()));
     }
 
     private <T> T extractClaim(String token, Function<Claims, T> claimsResolvers) {
@@ -46,16 +46,7 @@ public class JwtServiceImpl implements JwtService {
     private String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
         return Jwts.builder().setClaims(extraClaims).setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256).compact();
-    }
-
-    private boolean isTokenExpired(String token) {
-        return extractExpiration(token).before(new Date());
-    }
-
-    private Date extractExpiration(String token) {
-        return extractClaim(token, Claims::getExpiration);
     }
 
     private Claims extractAllClaims(String token) {
